3fc0f47165ce014e6a30d4ead2fdcd824f507866
w1shope <wishhopelee@gmail.com>
2024-05-30 17:45:45 +0900

    first commit

---
+++ src/main/java/com/example/cicd/AppController.java
@@ -0,0 +1,13 @@
+package com.example.cicd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
+}
